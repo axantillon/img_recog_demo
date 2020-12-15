48ea6e626133edfa9f7f23dcf2880f452c218bc2
Andres Antillon <aantillon.leiva@gmail.com>
2020-12-15 11:55:58 -0600

    1st commit

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+    <webcamApp/>
+    
+  </div>
+</template>
+
+<script>
+import webcamApp from "./components/webcam_app.vue"
+
+export default {
+
+name: 'App',
+  components: {
+    webcamApp
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ vue.config.js
@@ -0,0 +1,3 @@
+module.exports = {
+    publicPath: '/img_recog_demo/'
+  }
